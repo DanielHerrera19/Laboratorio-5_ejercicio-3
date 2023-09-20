96593b74cafc4c08b1617c8962f78ae9679845ce
Daniel <Daniel@DESKTOP-KKC0TQU>
2023-09-19 22:03:04 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_5_ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el numero de notas: ");
+            int cantidadenotas = int.Parse(Console.ReadLine());
+
+            int promedio = 0;
+
+            for (int i = 1; i <= cantidadenotas; i++)
+            {
+                Console.Write("Ingrese la nota " + i + ": ");
+                int nota = int.Parse(Console.ReadLine());
+                promedio = promedio + nota;
+            }
+            Console.WriteLine("El promedio es " + promedio/cantidadenotas);
+            Console.ReadKey();
+        }
+    }
+}
